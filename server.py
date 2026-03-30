88da75badb345fee6d856e3510e8d6d389ec074e
marcosarrietaoncomputer <marcosarrietamm@gmail.com>
2026-03-30 07:59:07 -0400

    added docstring and fixed some typos

--- server.py
+++ server.py
@@ -1,3 +1,8 @@
+"""
+This app serves the user to detect and extract emotions from a sentence,
+while also showcasing what's the dominant emotion
+"""
+
 from flask import Flask, render_template, request
 from EmotionDetection.emotion_detection import emotion_detector
 
@@ -7,20 +12,30 @@ app = Flask("Emotion detector")
 @app.route("/emotionDetector")
 def emot_detector():
     """ 
-    What the app does
+    Detects the emotion from the user input and returns
+    the pondered emotion detected plus the main emotion for the sentence
     """
 
     text_to_analyze = request.args.get('textToAnalyze')
     response = emotion_detector(text_to_analyze)
 
-    if response == None:
-        return f"Invalid text!. Please try again!"
+    if response is None:
+        return "Invalid text!. Please try again!"
 
-    return f"For the given statement, the system response is 'anger': {response['anger']}, 'disgust': {response['disgust']}, 'fear': {response['fear']}, 'joy': {response['joy']}, 'sadness': {response['sadness']}. The dominant emotion is {response['dominant_emotion']}"
+    return f"""For the given statement, the system response is
+    'anger': {response['anger']},
+    'disgust': {response['disgust']},
+    'fear': {response['fear']},
+    'joy': {response['joy']},
+    'sadness': {response['sadness']}.
+    The dominant emotion is {response['dominant_emotion']}"""
 
 
 @app.route("/")
 def render_index_page():
+    """
+    Renders the home page for the web
+    """
     return render_template('index.html')
 
 if __name__ == "__main__":
